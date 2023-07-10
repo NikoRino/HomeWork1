b5e146d2c58ad1b5c6e37b6999d759bb1927d0df
NikoRino <niko89rino@gmail.com>
2023-07-10 20:32:48 +0700

    Исправил третью задачу

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,7 +2,7 @@
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 % 2 == 0)
-    Console.Write($"Число четное");
+    Console.Write("Число четое");
 else
 {
     Console.WriteLine("Число не четное");
