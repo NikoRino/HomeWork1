b25ed9e818bff59f15e44bfe8fc0bd92dab1d5bb
NikoRino <niko89rino@gmail.com>
2023-07-10 21:12:25 +0700

    Внес четвертую задачу

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+n = Math.Abs(n);
+
+Console.WriteLine("Четные числа", n);
+
+for (int i = 2; i < n - 1; i = i + 2)
+{
+    Console.Write(i + ", ");
+}
+
+if (n % 2 == 0)
+    Console.Write(n);
+else
+    Console.Write(n - 1);
