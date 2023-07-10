49b8fc1444e2e99a0d15b006218abff0236ba450
NikoRino <niko89rino@gmail.com>
2023-07-10 19:20:39 +0700

    Ввел первое задание

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    Console.Write($"Первое число больше:{num1}");
+else
+{
+    Console.WriteLine($"Второе число больше:{num2}");
+}
