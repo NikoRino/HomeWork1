15a3f44d381fabdd4ac00a5b47f3503ac5b12965
NikoRino <niko89rino@gmail.com>
2023-07-10 20:17:54 +0700

    Ввел третью задачу

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+    Console.Write($"Число четное");
+else
+{
+    Console.WriteLine("Число не четное");
+}
