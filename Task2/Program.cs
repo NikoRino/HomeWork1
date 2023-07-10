72090ce9f02424958bc43472672e848fbf3ce15d
NikoRino <niko89rino@gmail.com>
2023-07-10 19:54:57 +0700

    Ввел задание второе

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    if (num1 > num3)
+        Console.WriteLine($"Первое число больше: {num1}");
+    else
+        Console.WriteLine($"Третье число больше: {num3}");
+else if (num2 > num1)
+    if (num2 > num3)
+        Console.WriteLine($"Второе число больше: {num2}");
+    else
+        Console.WriteLine($"Третье число больше: {num3}");
